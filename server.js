9316d97d838beb8f476b453d164f821aab3ad599
marco <rootslowed@gmail.com>
2021-03-24 18:20:09 -0300

    mongodb atlas database connected successfully

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// MONGOOSE
+mongoose.connect(MONGOURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => {
+    console.log("database connected");
+});
 
 // LISTEN
 app.listen(PORT, () => {
