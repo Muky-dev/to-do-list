42c9bd3a3ae700544993c889a6da8c2347f3b92c
Muky-dev <69209901+Muky-dev@users.noreply.github.com>
2021-03-26 19:16:37 -0300

    Add handlebars

--- server.js
+++ server.js
@@ -1,25 +1,39 @@
 // MODULES
 import express from "express";
 import mongoose from "mongoose";
+import handlebars from "express-handlebars";
 import dotenv from "dotenv";
-dotenv.config0;
+dotenv.config()
 
 // APP
 const app = express();
 const PORT = process.env.PORT || 3000;
 const MONGOURI = process.env.MONGOURI;
 
+app.use(express.urlencoded({ limit: "10mb", extended: false}))
+
+// HANDLEBARS
+app.engine('handlebars', handlebars({
+	defaultLayout: 'main',
+}));
+app.set('view engine', 'handlebars');
+
 // ROUTES
 import rootRouter from "./routes/root.js";
-
+app.use("/", rootRouter);
 
 // MONGOOSE
 mongoose.connect(MONGOURI, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+db.on('error', error => console.error(error));
+db.once('open', () => {
+    console.log("database connected");
+});
 
 // LISTEN
 app.listen(PORT, () => {
-	console.log(`server running: localhost/${PORT}`);
+	console.log(`server running: localhost:${PORT}`);
 });
